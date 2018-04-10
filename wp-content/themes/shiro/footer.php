03b561ea054f06ea83eeb48696743c44bb9b8755
peterbrionibinex <peter.br@ibinex.tech>
2018-04-10 15:48:58 +0800

    stuff

--- wp-content/themes/shiro/footer.php
+++
@@ -1,7 +0,0 @@
-		</div><!-- END container-->
-	</div><!-- END wrapper -->	
-</div><!-- END #page -->
-<?php wp_footer(); ?>
-
-</body>
-</html>
--- wp-content/themes/shiro/sidebar.php
+++
@@ -1,6 +0,0 @@
-	<?php if ( is_active_sidebar( 'sidebar' ) ) : ?>
-		<div id="secondary" class="widget-area" role="complementary">
-			<?php dynamic_sidebar( 'sidebar' ); ?>
-		</div><!-- END #secondary .widget-area-->
-	<?php endif; ?>
-</div><!-- END #main -->
